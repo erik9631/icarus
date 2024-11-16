773990fbcfca173a56942b52f08a28414e69d0b0
erik9631 <erik9631@gmail.com>
2024-11-16 18:55:04 +0100

    feat: Finalized thread loop

--- src/executor.rs
+++ src/executor.rs
@@ -26,20 +26,29 @@ pub struct Executor {
     waker_mutex: Mutex<bool>,
     join_handles: Vec<JoinHandle<()>>,
 }
+
+struct UnsafePtrExecutor(*const Executor);
+unsafe impl Send for UnsafePtrExecutor {}
 impl Executor {
-    fn thread_loop(&self, thread_queues: Vec<ArrayQueue<Task>>, thread_id: usize, thread_waker: &Condvar, waker_mutex: &Mutex<bool>) {
+    fn thread_loop(executor: UnsafePtrExecutor, thread_id: usize) {
         THREAD_LOCAL_ID.set(thread_id);
         let local_id = THREAD_LOCAL_ID.get();
+        println!("Thread loop {} started", thread_id);
+        let executor = unsafe { &*executor.0 };
+        let thread_queues = &executor.thread_queues;
+        let thread_waker = &executor.thread_waker;
+        let waker_mutex = &executor.waker_mutex;
 
         loop {
             // First take tasks from its own queue
             while let Some(task) = thread_queues[local_id].pop() {
-                (task.func)(&self);
+                (task.func)(&executor);
             }
 
             // Then take tasks from other queues
             for queue in thread_queues.iter() {
                 if let Some(task) = queue.pop() {
+                    println!("Thread {} stealing task {} to queue", thread_id, task.id);
                     if let Err(_) = thread_queues[local_id].push(task) {
                         panic!("Failed to push to local queue");
                     }
@@ -54,28 +63,42 @@ impl Executor {
                 .expect("Thread waker failed to park");
             // Drain the queue
             if !state {
+                println!("Thread loop {} draining", thread_id);
                 while let Some(task) = thread_queues[local_id].pop() {
-                    (task.func)(&self);
+                    (task.func)(&executor);
                 }
+                println!("Thread loop {} finished", thread_id);
                 break;
             }
         }
     }
-    pub fn new(thread_count: usize) -> Self {
-        let mut executor = Self {
+    pub fn new(thread_count: usize) -> Box<Self> {
+        let thread_loops = thread_count - 1;
+        let mut executor = Box::new(Self {
             thread_queues: Vec::with_capacity(thread_count),
             id_gen: AtomicUsize::new(0),
             thread_waker: Condvar::new(),
             waker_mutex: Mutex::new(true),
             join_handles: Vec::with_capacity(thread_count),
-        };
+        });
         for _ in 0..thread_count {
             executor.thread_queues.push(ArrayQueue::new(1024));
+        }
+
+        unsafe {
+            for i in 0..thread_loops {
+                let executor_ptr = UnsafePtrExecutor(&*executor as *const Executor);
+                let id = i + 1;
+                executor
+                    .join_handles
+                    .push(std::thread::spawn(move || Self::thread_loop(executor_ptr, id)));
+            }
         }
         executor
     }
     pub fn submit(&self, func: Box<dyn FnOnce(&Executor) + Send>) -> usize {
         let result = self.id_gen.load(std::sync::atomic::Ordering::Acquire);
+        println!("Submitting task {} to thread {}", result, THREAD_LOCAL_ID.get());
         self.thread_queues[THREAD_LOCAL_ID.get()].push(Task::new(func, result)).ok();
         self.thread_waker.notify_all();
         self.id_gen.fetch_add(1, std::sync::atomic::Ordering::AcqRel);
@@ -86,6 +109,10 @@ impl Executor {
         let mut guard = self.waker_mutex.lock().unwrap();
         *guard = false;
         self.thread_waker.notify_all();
+        drop(guard);
+        //Close should yield till empty
+        let executor_ptr = UnsafePtrExecutor(&*self as *const Executor);
+        Self::thread_loop(executor_ptr, 0);
 
         for handle in self.join_handles.drain(..) {
             handle.join().unwrap();
